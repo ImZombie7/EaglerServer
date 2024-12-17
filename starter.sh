dff5443adaa65a81269bbe23db61309f8d356ecb
ImZombie7 <157273060+ImZombie7@users.noreply.github.com>
2024-12-17 16:08:34 +0530

    Update starter.sh
    all ok

--- starter.sh
+++ starter.sh
@@ -1,1 +1,2 @@
+cd Waterfall
  java -Xms4096M -Xmx4096M -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -jar waterfall.jar --nogui
